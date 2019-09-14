5ad80d06dd4d15bba75723aed8641406da2ef063
Gilvan Leal <gilvan.sleal@gmail.com>
2019-09-14 17:47:35 -0300

    Ordem dos seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(RecourseTableSeeder::class);
         $this->call(ItemTableSeeder::class);
         $this->call(SurvivorTableSeeder::class);
+        $this->call(RecourseTableSeeder::class);
     }
 }
