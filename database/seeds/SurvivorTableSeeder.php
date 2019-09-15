8dcac7b89021366cd863195dff107d20b6a30c0b
Gilvan Leal <gilvan.sleal@gmail.com>
2019-09-15 12:37:04 -0300

    [ADD] ReporteInfeted seeder

--- database/seeds/SurvivorTableSeeder.php
+++ database/seeds/SurvivorTableSeeder.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Seeder;
 use App\Survivor;
 use App\Recourse;
+use App\Item;
 
 class SurvivorTableSeeder extends Seeder
 {
@@ -21,23 +22,31 @@ class SurvivorTableSeeder extends Seeder
         Survivor::create(['name' => 'Gilvan Leal', 'birth' => '1990-10-19',
          'gender' => 'male', 'latitude'=> 10.123456, 'longitude'=> 20.654321
          ])->recourses()->createMany([
-            ['amount' => 2, 'item_id' => 1],
-            ['amount' => 1, 'item_id' => 3],
-            ['amount' => 4, 'item_id' => 4],
+            ['amount' => 2, 'item_id' => Item::where('name', 'Water')->value('id')],
+            ['amount' => 1, 'item_id' => Item::where('name', 'Medication')->value('id')],
+            ['amount' => 4, 'item_id' => Item::where('name', 'Ammunition')->value('id')]
          ]
         );
         Survivor::create(['name' => 'Dywlly Porto', 'birth' => '1985-11-08',
          'gender' => 'female', 'latitude'=> 50.123456, 'longitude'=> 90.654321
          ])->recourses()->createMany([
-            ['amount' => 1, 'item_id' => 1],
-            ['amount' => 2, 'item_id' => 3]
+            ['amount' => 1, 'item_id' => Item::where('name', 'Water')->value('id')],
+            ['amount' => 2, 'item_id' => Item::where('name', 'Medication')->value('id')]
          ]
         );
         Survivor::create(['name' => 'João Paulo', 'birth' => '1999-12-5',
          'gender' => 'male', 'latitude'=> 50.123456, 'longitude'=> 90.654321
          ])->recourses()->createMany([
-            ['amount' => 1, 'item_id' => 1],
-            ['amount' => 2, 'item_id' => 3]
+            ['amount' => 1, 'item_id' => Item::where('name', 'Water')->value('id')],
+            ['amount' => 2, 'item_id' => Item::where('name', 'Medication')->value('id')]
+         ]
+        );
+
+        Survivor::create(['name' => 'Paulo Silva', 'birth' => '100-12-5',
+         'gender' => 'male', 'latitude'=> 50.123456, 'longitude'=> 90.654321
+         ])->recourses()->createMany([
+            ['amount' => 10, 'item_id' => Item::where('name', 'Food')->value('id')],
+            ['amount' => 2, 'item_id' => Item::where('name', 'Ammunition')->value('id')]
          ]
         );
 
